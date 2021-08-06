d3505207cdc546dd65af98267c97234bb9f549b8
Santhoshkumaradoss <87718356+Santhoshkumaradoss@users.noreply.github.com>
2021-08-06 09:33:21 +0530

    Delete Basics day - 5 directory

--- Basics day - 5/for demo 2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int i;
-    for(i=6;i<=15;i+=3)
-    {
-        printf("%d\n",i);
-    }
-}
--- Basics day - 5/for demo 4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int i;
-    for(i=8;i>=80;i+=8)
-    {
-        printf("%d",i);
-    }
-}
