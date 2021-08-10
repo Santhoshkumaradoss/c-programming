5dc6337926eaee07ad19502bfa95392e5de5a9ea
Santhoshkumaradoss <87718356+Santhoshkumaradoss@users.noreply.github.com>
2021-08-10 22:48:06 +0530

    Delete Day 10 Array demo directory

--- Day 10 Array demo/Array demo 1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a[5]={1,2,3,4,5};
-    printf("%d",a[0]+a[4]);
-}
--- Day 10 Array demo/Array demo 2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a[5]={1,2,3,4,5},i;
-    for(i=0;i<5;i++)
-    {
-        printf("\n%d",a[i]);
-    }
-}
